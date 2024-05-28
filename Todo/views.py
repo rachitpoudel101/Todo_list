fafd3cae8c425712019dc3a915c4518143c52be0
rachitpoudel101 <rachit300poudel@gmail.com>
2024-05-28 22:25:28 +0545

    Add the user signup form and password auth  in django

--- Todo/views.py
+++ Todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 def signupuser(request):
-    return render(request, 'Todo/signupuser.html',{'form':UserCreationForm()})
-
+    if request.method == 'GET':
+        return render(request, 'Todo/signupuser.html',{'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['passwprd2']:
+            User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            User.save()
+        else:
+            print("tell the user the paswrod not match.")
