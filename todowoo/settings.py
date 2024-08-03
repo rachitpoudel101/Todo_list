050acf4a13fc2e2eb7405741f4a4c9dc8c95ed17
rachitpoudel101 <rachit300poudel@gmail.com>
2024-08-03 18:30:48 +0545

    updated requrement.txt

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_jjhm#&qjqbm_qula%xs2(0&^qwai@2c%au&4z57%g+=-lsbno'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
